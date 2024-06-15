ca80d4897d0a79b62341bf921e3d867fb2f1a35a
Rayne <niellaycano5346@gmail.com>
2024-06-15 16:00:37 +0800

    Add files via upload

---
+++ web/js/one.js
@@ -0,0 +1,7 @@
+/* ==================== Typing Animation ======================== */
+var typed = new Typed(".typing", {
+    strings:["","taking Computer Science Course", "learning Web Development", "learning Graphics Design", "learning Web Design"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
